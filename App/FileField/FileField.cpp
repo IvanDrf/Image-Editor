e932963b25a3dcafd4b7ef6d8cec30d50f1a393c
IvanDrf <ivan@IvanComp.>
2025-03-04 13:23:04 +0300

    FileField

---
+++ App/FileField/FileField.cpp
@@ -0,0 +1,51 @@
+#include "FileField.hpp"
+
+#include "../MainWindow/MainWindow.hpp"
+
+FileField::FileField() {
+    if (!font_.loadFromFile("../WindowFiles/open-sans-semibold.ttf")) {
+        throw std::runtime_error("Font could not be uploaded");
+    }
+
+    shape_.setSize(sf::Vector2f(kFileFieldWidth, kFileFieldHeight));
+    shape_.setFillColor(sf::Color(83, 83, 83));
+    shape_.setOutlineColor(sf::Color::Black);
+    shape_.setOutlineThickness(3);
+
+    shape_.setPosition(0, kMainWindowHeight * 0.045f);
+}
+
+void FileField::DrawField(sf::RenderWindow& window) {
+    window.draw(shape_);
+
+    sf::Text filesNames;
+    filesNames.setFont(font_);
+    filesNames.setCharacterSize(kCharacterSize);
+    filesNames.setFillColor(sf::Color::White);
+
+    float distanceBetweenFiles{kMainWindowHeight * 0.1f};
+    for (const auto& fileName : files_) {
+        filesNames.setString(fileName);
+
+        filesNames.setPosition(10, distanceBetweenFiles);
+        distanceBetweenFiles += 30;
+
+        window.draw(filesNames);
+    }
+}
+
+void FileField::AddFile(const std::string& fileName) {
+    files_.emplace_back(fileName);
+}
+
+void FileField::DeleteFile(const std::string& fileName) {
+    if (std::find(files_.begin(), files_.end(), fileName) == files_.end()) {
+        return;
+    }
+
+    files_.erase(std::remove(files_.begin(), files_.end(), fileName), files_.end());
+}
+
+const std::vector<std::string>& FileField::GetFiles() const {
+    return files_;
+}
