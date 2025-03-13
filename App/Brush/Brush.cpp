00c977fac3524d95b85b66acfa4ef1b1dd6104cb
IvanDrf <ivan@IvanComp.>
2025-03-14 00:04:53 +0300

    added new methods and inheritance

--- App/Brush/Brush.cpp
+++ App/Brush/Brush.cpp
@@ -1,6 +1,6 @@
 #include "Brush.hpp"
 
-Brush::Brush(const float radius, const sf::Color& color): radius_(radius), color_(color) {
+Brush::Brush(const float radius, const sf::Color& color) : radius_(radius), color_(color) {
     shape_.setRadius(radius);
     shape_.setFillColor(color);
 
@@ -14,16 +14,20 @@ void Brush::SetRadius(const float newRadius) {
     shape_.setOrigin(newRadius, newRadius);
 }
 
+float Brush::GetRadius() const {
+    return radius_;
+}
+
 void Brush::SetColor(const sf::Color& newColor) {
     color_ = newColor;
     shape_.setFillColor(newColor);
 }
 
+sf::Color Brush::GetColor() const {
+    return color_;
+}
+
 void Brush::Draw(sf::RenderTexture& texture, const sf::Vector2f& position) {
     shape_.setPosition(position);
     texture.draw(shape_);
-}
-
-float Brush::GetSize() const {
-    return radius_;
 }
--- App/Brush/Brush.hpp
+++ App/Brush/Brush.hpp
@@ -1,20 +1,27 @@
 #pragma once
 
 #include "SFML/Graphics.hpp"
+#include "../InputField/InputField.hpp"
+#include "../Image/Image.hpp"
 
-class Brush {
+namespace {
+const float kBrushInitialRadius{10.0f}; // Initial radius for brush
+}
+
+class Brush : public Image, public InputField {
  private:
     sf::CircleShape shape_;
     sf::Color color_;
     float radius_;
+
  public:
     Brush(const float radius, const sf::Color& color);
 
     void SetRadius(const float newRadius);
+    float GetRadius() const;
+
     void SetColor(const sf::Color& newColor);
-
+    sf::Color GetColor() const;
     void Draw(sf::RenderTexture& texture, const sf::Vector2f& position);
-    float GetSize() const;
-
 
 };
