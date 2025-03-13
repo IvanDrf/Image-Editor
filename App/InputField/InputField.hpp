0d1f19753333811facb1bda04cb99c6815da7f25
IvanDrf <ivan@IvanComp.>
2025-03-14 00:17:53 +0300

    added input field files

---
+++ App/InputField/InputField.hpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+
+namespace {
+const short KEnter{13};
+const short kBackSpace{8};
+}  // namespace
+
+class InputField {
+ private:
+    sf::RectangleShape box_;
+    sf::Font font_;
+    sf::Text text_;
+    std::string inputString_;
+
+ public:
+    InputField();
+    InputField(const float x, const float y, const sf::Vector2f& size);
+
+    void SetTextPosition(const float x, const float y);
+    void SetFont(const sf::Font& font);
+    void SetText(const std::string& text);
+
+    void SetInputText(const std::string& inputString);
+
+    void SetBoxPosition(const float x, const float y);
+    void SetBoxSize(const sf::Vector2f& size);
+
+    std::string Input(sf::Event& event, sf::RenderWindow& window);
+    static std::string CreateInputWindow(const std::string& heading, const std::string& inputText);
+
+    void Clear();
+    void Draw(sf::RenderWindow& window) const;
+};
