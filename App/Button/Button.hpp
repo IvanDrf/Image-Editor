1635a661b374f791f5360395dc238ff154c0ab3d
IvanDrf <ivan@IvanComp.>
2025-04-01 00:10:48 +0300

    fix: clang-format

--- App/Button/Button.hpp
+++ App/Button/Button.hpp
@@ -14,9 +14,9 @@ class Button {
     Button(const float x, const float y, const std::string& name, const sf::Color& color, const sf::Font& font);
     static void CreateMenuButtons(std::vector<Button>& buttons, const std::vector<std::string>& names, const std::vector<sf::Color>& colors, const sf::Font& font);
 
-   void SetSize(const sf::Vector2f& size);
-   void SetOutlineThickness(const float thickness);
-   void SetOutlineColor(const sf::Color& color);
+    void SetSize(const sf::Vector2f& size);
+    void SetOutlineThickness(const float thickness);
+    void SetOutlineColor(const sf::Color& color);
 
     void DrawButton(sf::RenderWindow& window) const;
     void SetColor(const sf::Color& newColor);
