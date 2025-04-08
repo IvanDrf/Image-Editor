b8525ffd25404360237242dec9ee4876a44b41f3
IvanDrf <ivandrgmr@gmail.com>
2025-04-09 00:10:17 +0300

    fix: clang-format fix

--- App/InputWindow/InputWindow.hpp
+++ App/InputWindow/InputWindow.hpp
@@ -3,7 +3,7 @@
 #include <string>
 
 namespace Window {
-std::string AddFileWindow(); // Add file button
+std::string AddFileWindow();  // Add file button
 
-std::string SaveFileWindow(const std::string& fileName); // Save file button
-}
+std::string SaveFileWindow(const std::string& fileName);  // Save file button
+}  // namespace Window
