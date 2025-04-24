93d066e9b2f1f2e8aee8854d69b5595318b59a2e
IvanDrf <ivandrgmr@gmail.com>
2025-04-24 21:31:15 +0300

    ref: CreateMenuButtons->CreateFileButtons

--- App/Button/Button.hpp
+++ App/Button/Button.hpp
@@ -12,7 +12,7 @@ class Button {
 
  public:
     Button(const float x, const float y, const std::string& name, const sf::Color& color, const sf::Font& font);
-    static void CreateMenuButtons(std::vector<Button>& buttons, const std::vector<std::string>& names, const std::vector<sf::Color>& colors, const sf::Font& font);
+    static void CreateFileButtons(std::vector<Button>& buttons, const std::vector<std::string>& names, const std::vector<sf::Color>& colors, const sf::Font& font);
 
     void DrawButton(sf::RenderWindow& window) const;
     void SetColor(const sf::Color& newColor);
