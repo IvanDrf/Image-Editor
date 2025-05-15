61755c9d4b6d5f073a632772d8b0a0d376f19c58
IvanDrf <ivandrgmr@gmail.com>
2025-05-16 00:33:28 +0300

    fix: delete useless files

--- App/InputWindow/InputWindow.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace Window {
-std::string AddFileWindow();  // Add file button
-
-std::string SaveFileWindow(const std::string& fileName);  // Save file button
-}  // namespace Window
